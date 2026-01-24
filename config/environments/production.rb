84e9633e5f53d954e393d3c041c8230d2fa2f63c
Pawel <pawel@przylucki.pl>
2026-01-24 01:31:27 +0100

    CHG clean up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << 'pennylane.kong.pl'
+  config.hosts << 'knog.pl'
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  # config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!).
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
